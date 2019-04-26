3c3e226c3647eb8cefdf9ec6b83d4c934d47afa0
Ethan Dickey <etdickey@gmail.com>
2019-04-26 09:36:42 -0500

    Updated the generate data function and running code instructions to include how to run the python generateDataWith.py script.

--- generateDataWith.py
+++ generateDataWith.py
@@ -1,9 +1,11 @@
 #!/bin/python
 #run with python3 generateDataWith.py > inputFile.in
 import random
+import sys
 
-NUM_POINTS = 20
-MAX_X = 100
-MAX_Y = 100
+NUM_POINTS = int(sys.argv[1])
+MAX_X = int(sys.argv[2])
+MAX_Y = int(sys.argv[3 if len(sys.argv) == 4 else 2])
+
 for i in range(0, NUM_POINTS):
     print(str(random.randint(0,MAX_X)) + " " + str(random.randint(0,MAX_Y)))
